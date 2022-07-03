89504ef3f207012f03754d4c0da93e7aabdf02fb
Tiago <tiagobsb84@gmail.com>
2022-07-03 15:42:45 -0300

    Criando projeto

---
+++ src/main/java/br/com/tiago/mvc/MudiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiago.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiago/mvc/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiago.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

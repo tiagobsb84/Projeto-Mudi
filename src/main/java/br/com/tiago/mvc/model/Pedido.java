3fa6817f82c5152bd75aa32ba025d44b599b5c89
Tiago <tiagobsb84@gmail.com>
2022-07-05 19:57:46 -0300

    Configurado o banco de dados Postgres

--- src/main/java/br/com/tiago/mvc/model/Pedido.java
+++ src/main/java/br/com/tiago/mvc/model/Pedido.java
@@ -3,8 +3,20 @@ package br.com.tiago.mvc.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido")
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataDaEntrega;

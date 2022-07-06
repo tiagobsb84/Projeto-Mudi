27c1386a7bdc5207a419f1dc314fbbfe122e5816
Tiago <tiagobsb84@gmail.com>
2022-07-06 16:57:53 -0300

    Validacao do formulario

---
+++ src/main/java/br/com/tiago/mvc/controller/PedidoController.java
@@ -0,0 +1,39 @@
+package br.com.tiago.mvc.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.tiago.mvc.dto.RequisicaoNovoPedido;
+import br.com.tiago.mvc.model.Pedido;
+import br.com.tiago.mvc.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping("formulario")
+	public String formulario(RequisicaoNovoPedido requisicao) {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		return "pedido/formulario";
+	}
+}
---
+++ src/main/java/br/com/tiago/mvc/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,52 @@
+package br.com.tiago.mvc.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import br.com.tiago.mvc.model.Pedido;
+
+public class RequisicaoNovoPedido {
+
+	@NotBlank
+	private String nomeProduto;
+	
+	@NotBlank
+	private String urlProduto; 
+	
+	@NotBlank
+	private String urlImagem ; 
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setDescricao(descricao);
+		pedido.setUrlProduto(urlProduto);
+		pedido.setUrlImagem(urlImagem);
+		return pedido;
+	}
+}

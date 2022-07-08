de6830b7e813e83af758fd05eab7bdefeff5b3cd
Tiago <tiagobsb84@gmail.com>
2022-07-08 01:31:14 -0300

    Rotas do status

--- src/main/java/br/com/tiago/mvc/repository/PedidoRepository.java
+++ src/main/java/br/com/tiago/mvc/repository/PedidoRepository.java
@@ -1,11 +1,16 @@
 package br.com.tiago.mvc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.tiago.mvc.model.Pedido;
+import br.com.tiago.mvc.model.StatusPedidos;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedidos valueOf);
 	
 }
